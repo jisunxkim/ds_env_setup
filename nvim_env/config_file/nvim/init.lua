b94bc42fccca064b57e7f0daa357e51afa02663c
Jisun Kim <jisukim@zulily.com>
2023-04-23 00:04:34 -0700

    fixed LSP plugin in init.lua, and added LSPs.

--- nvim_env/config_file/nvim/init.lua
+++ nvim_env/config_file/nvim/init.lua
@@ -7,10 +7,23 @@ require("jisun.plugins.nvim-tree")
 require("jisun.plugins.lualine")
 require("jisun.plugins.telescope")
 require("jisun.plugins.nvim-cmp")
-require("jisun.plugins.lsp.mason")
+--require("jisun.plugins.lsp.mason")
 require("jisun.plugins.lsp.lspsaga")
-require("jisun.plugins.lsp.lspconfig")
+--require("jisun.plugins.lsp.lspconfig")
 require("jisun.plugins.lsp.null-ls")
 require("jisun.plugins.autopairs")
 require("jisun.plugins.treesitter")
 require("jisun.plugins.gitsigns")
+
+-- my collection
+require("lspconfig").jedi_language_server.setup({})
+require("lspconfig").pyright.setup({})
+require("lspconfig").java_language_server.setup({})
+
+require("lspconfig").dockerls.setup({})
+
+require("lspconfig").jsonls.setup({
+	capabilities = capabilities,
+})
+require("lspconfig").yamlls.setup({})
+require("lspconfig").sqlls.setup({})
